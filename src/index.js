10a3c346c4bcf2660194de65ba554c26fe47511e
Markus Bjerre (lgv900) <markus.bjerre@teliacompany.com>
2020-05-28 23:36:42 +0200

    Rename HelloWorld to suit three configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Test import of a JavaScript function, an SVG, and Sass
-import { HelloWorld } from './js/HelloWorld';
+import { MyScene } from './js/MyScene';
 import WebpackLogo from './images/webpack-logo.svg';
 import './styles/index.scss';
 
@@ -9,7 +9,7 @@ logo.src = WebpackLogo;
 
 // Create heading node
 const greeting = document.createElement('h1');
-greeting.textContent = HelloWorld();
+greeting.textContent = MyScene();
 
 // Append SVG and heading nodes to the DOM
 const app = document.querySelector('#root');
---
+++ src/js/MyScene.js
@@ -0,0 +1,1 @@
+export const MyScene = () => 'Hello, World!';
