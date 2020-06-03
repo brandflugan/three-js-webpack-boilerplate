d6c222ba92c18d744042a043e04b4aa03fa4fff5
Guillaume Fradin <guillaumefradin@gmail.com>
2020-06-03 17:08:18 +0100

    made stl loader synchronous with a promise

--- src/js/box/outer/geometry.js
+++ src/js/box/outer/geometry.js
@@ -3,9 +3,11 @@ import { STLLoader } from '../../../../node_modules/three/examples/jsm/loaders/S
 import WebpackCube from '../../../models/webpack-cube.stl';
 
 export const geometry = () => {
-    const loader = new STLLoader();
-    loader.load(WebpackCube, (geometry) => {
-        console.log(geometry);
-        return geometry;
+    return new Promise((resolve) => {
+        const loader = new STLLoader();
+        loader.load(WebpackCube, (geometry) => {
+            console.log(geometry);
+            resolve(geometry);
+        });
     });
 };
